3c71578ce81fdfea7513bc45caecd9f4b79f037c
Damian Rudzinski <damian.rudzinski@arcelormittal.com>
2020-02-01 17:19:06 +0100

    Initialize repository, add first all project

---
+++ ESET_recruitment_TASK/ESET_recruitment_TASK.cpp
@@ -0,0 +1,74 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+class directory
+{
+private:
+	string path;
+
+	bool PathExists;
+
+	int NumOfFiles;
+	int NumOftextFiles;
+	int NumOfFolders;
+public:
+	void InsertPath()
+	{
+		cin >> this->path;
+		if (fs::exists(this->path))
+		{	
+			this->PathExists = true;
+			int i = 0;
+			for (auto& entry : fs::directory_iterator(path))
+				(
+
+					cout << entry.is_directory() << endl
+
+					);
+		}
+		else
+		{	
+			this->PathExists = false;
+			cout << "Path did not found";
+		}
+	};
+};
+
+class File
+{
+private:
+	string name;
+	int characters;
+	bool txtFile;
+	string FindText;
+public:
+	void InsertText()
+	{
+		cin >> this->FindText;
+	}
+};
+
+
+int main()
+{	
+	directory D1;
+	File f1;
+    cout << "Insert path to file or folder\n"; 
+	cout << "Path:";
+	D1.InsertPath();
+
+	cout << "\nInsert text to be found\n";
+	cout << "Text:";
+	f1.InsertText();
+
+
+
+
+
+}
+
