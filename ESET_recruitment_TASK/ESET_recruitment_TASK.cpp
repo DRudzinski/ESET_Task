ec741c5f09f95d1808ea2ca93469cd43ba13b5b4
Damian Rudzinski <damian.rudzinski@arcelormittal.com>
2020-02-04 13:21:48 +0100

    Implement \t and \n rules in ExploreFile method

--- ESET_recruitment_TASK/ESET_recruitment_TASK.cpp
+++ ESET_recruitment_TASK/ESET_recruitment_TASK.cpp
@@ -1,9 +1,9 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <string>
-#include <filesystem>
-#include <queue>
-#include <fstream>
+#include <filesystem> //Library necessery to use find. method 
+#include <queue> //Necessery to implement queue
+#include <fstream> // Use to open file end get all text
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -19,26 +19,28 @@ public:
 	//Method to type initial path and check if path exists and if refers to directory or txt file
 	int InPath()
 	{
+		//Get initial path
 		cout << "Insert path to file or directory" << endl;
 		cin >> this->InputPath;
 
 
+		//Validate if path lead to txt file or directory
 		if (fs::exists(this->InputPath))
 		{
 			string rPath = InputPath.substr(InputPath.length() - 4, InputPath.length());
 			if (rPath == ".txt")
 			{
-				return 0;
+				return 0; //if path lead to txt file
 			}
 			else
 			{
-				return 1;
+				return 1; //if path lead to directory
 			}
 		}
 		else
 		{	
 			cout << "Path nit found";
-			return -100;
+			return -100; //if path not exists
 		}
 	}
 	//public method to reuurn full string with path
@@ -50,8 +52,10 @@ public:
 	bool InputText()
 	{	
 		cout << "Insert text to be found" << endl;
-		cin >> this->TextToFind;
+		cin >> this->TextToFind; //Get text to be found
 
+
+		//Chceck if text is lower then 128 charcters
 		if (this->TextToFind.length() > 128)
 		{
 			cout << "Inserted text is too long, max availabe length: 128 characters " << endl;
@@ -65,11 +69,14 @@ public:
 			return this->ValidTextToFind;
 		}
 	}
+	//Return text to be found
 	string RetTxT()
 	{
 		return this->TextToFind;
 	}
 };
+//----------------------------------------------
+//----------------End of Path Class------------
 
 //Class for one directory
 class directory: public Path
@@ -83,10 +90,10 @@ public:
 	queue <string> FilesQueue; //Store queue in directory with path to txt files
 
 
-	//Method to check initial path
+	//Method to explore initial path
 	void FindTxtFiles(string Path)
 	{
-
+		//Looking for txt files
 		for (auto& entry : fs::directory_iterator(Path))
 		{	
 			string rPath = entry.path().u8string();
@@ -99,6 +106,10 @@ public:
 
 	}
 };
+//----------------------------------------------
+//------------End of Directory Class------------
+
+
 
 //Class for one file
 class File: public Path
@@ -126,16 +137,20 @@ public:
 
 		return this->name;
 	}
+
+	//Method to expore txt file and looking for matches
 	void ExplorFile(string Inpath,string TxTtoFind)
 	{
 		fstream file;
 
 		file.open(Inpath, ios::in);
-		cout << Path::RetPath();
+
 		if (file.good())
 		{
+			//--------------------------------------------
+			//--------------Set variables-----------------
 			string line; //Store one line from txt file
-			string Prevline; //Store Previous line
+			string Prevline=""; //Store Previous line
 
 			int charNum=0; // Stor sum of character in txt file
 
@@ -148,93 +163,183 @@ public:
 			bool NextLineCout = false; //Refers if suffix require characters from next line
 			int NextLineToCout = 0; //refers how many characters from next line should be display
 
+			size_t offset = 0; //Offset for Find. method
+
+			//--------------------------------------------
+			//-------------MAIN LOOP----------------------
+			//--------------------------------------------
 			while (getline(file, line))
 			{	
-				line = line + "\\n";
-
+				//--------------------------------------------
+				//Display missing suffix characters when match is at the end of the line
 				if (NextLineCout == true)
 				{	
-					cout << line.substr(0, NextLineToCout) << endl;
+					string tempSuffix = line.substr(0, NextLineToCout);
+					//Check if there are tabulators
+					offset = 0;
+					//Looking for tabulator in suffix
+					while ((tempSuffix.find("\t", offset) != string::npos))
+					{
+						size_t tab_poz = tempSuffix.find("\t", offset);
+
+						switch (tab_poz)
+						{
+						case 0: tempSuffix = "\\t" + tempSuffix.substr(1, tempSuffix.length()); //if tabulator appear as 1 char of suffix eg: \ta
+							break;
+						case 1: tempSuffix = tempSuffix.substr(0, tab_poz) + "\\t" + tempSuffix.substr(tab_poz + 1, tempSuffix.length()); //if tabulator appear as 2 char of suffix eg: a\t
+							break;
+						case 2: tempSuffix = tempSuffix.substr(0, tab_poz) + "\\t" + tempSuffix.substr(tab_poz + 1, tempSuffix.length()); //if tabulator appear as 3 char of suffix eg: \t\t
+							break;
+						default:
+							break;
+
+						}
+						offset = tab_poz +2;
+					}
+
+					cout << tempSuffix << endl;
 					NextLineCout = false;
-				}
+				}//Display missing characters end
+				//--------------------------------------------
 
 
-				occur = line.find(TxTtoFind);
+				occur = line.find(TxTtoFind); //Looking for a match in current line
+
+				//If match has been found
 				if (occur != string::npos)
 				{	
-
+					//--------------------------------------------
+					//--------------------------------------------
 					//Set Preffix start here
 					if (occur > 2)
 					{
-						preffix = line.substr(occur - 3, 3);
+						preffix = line.substr(occur - 3, 3); //If there is enough characters in current line
 					}
-					else
+					else if (Prevline!="")
 					{	
 						switch(occur)
 						{
 							case 0:
-								preffix = Prevline.substr(Prevline.length() - 3, 3);
+								preffix = Prevline.substr(Prevline.length() - 2, 2)+"\\n"; //if match starts the line aa\n
 								break;
 							case 1:
-								preffix = Prevline.substr(Prevline.length() - 2, 2) + line.substr(0, 1);
+								preffix = Prevline.substr(Prevline.length() - 1, 1) + "\\n"+ line.substr(0, 1); //If before match and line start there is 1 character eg a\na
 								break;
 							case 2:
-								preffix = Prevline.substr(Prevline.length() - 1, 1) + line.substr(0, 2);
+								preffix = "\\n" + line.substr(0, 2);//If before match and line start there are 2 characters eg \naa
 								break;
 							default:
 								break;
 						}
 
 					}
+
+					offset = 0;
+					//Looking for tabulator in preffix
+					while (preffix.find("\t",offset) != string::npos)
+					{
+						size_t tab_poz = preffix.find("\t");
+						switch (tab_poz)
+						{
+						case 0: preffix = "\\t" + preffix.substr(1, preffix.length()); //if tabulator appear as 1 char of prefix eg: \taa
+							break;
+						case 1: preffix = preffix.substr(0, tab_poz) + "\\t" + preffix.substr(tab_poz + 1, preffix.length());//if tabulator appear as 2 char of prefix eg: a\ta
+							break;
+						case 2: preffix = preffix.substr(0, tab_poz) + "\\t" + preffix.substr(tab_poz + 1, preffix.length());//if tabulator appear as 3 char of prefix aa\t
+							break;
+						case 3: preffix = preffix.substr(0, tab_poz) + "\\t" + preffix.substr(tab_poz + 1, preffix.length());//if tabulator appear as 3 char of prefix \ta\t
+							break;
+						case 4: preffix = preffix.substr(0, tab_poz) + "\\t" + preffix.substr(tab_poz + 1, preffix.length());//if tabulator appear as 3 char of prefix \t\t\t
+							break;
+						default:
+							break;								
+						}
+						offset = tab_poz+2;
+					}
 					// Setting preffix end here
-					
+					//--------------------------------------------
+					//--------------------------------------------
+
 					//Set Suffix start here
-					int CharAfterOccur = line.length() - (occur + TxTtoFind_Length);
+					int CharAfterOccur = line.length() - (occur + TxTtoFind_Length); //Chceck how many characters there are after match
 
 					if (CharAfterOccur > 2)
 					{
-						suffix = line.substr((occur + TxTtoFind_Length), 3);
+						suffix = line.substr((occur + TxTtoFind_Length), 3); //If there are enough character after match
+						NextLineToCout = 0;
 					}
 					else
 					{
 						switch(CharAfterOccur)
 						{
 						case 2:
-							suffix = line.substr((occur + TxTtoFind_Length), 2);
+							suffix = line.substr((occur + TxTtoFind_Length), 2)+"\\n"; //If there are 2 character after match
+							break;
+						case 1:
+							suffix = line.substr((occur + TxTtoFind_Length), 1)+"\\n"; //If there are 1 character after match
 							NextLineCout = true;
 							NextLineToCout = 1;
 							break;
-						case 1:
-							suffix = line.substr((occur + TxTtoFind_Length), 1);
+						case 0:
+							suffix = "\\n"; //If the match is in the end of the line
 							NextLineCout = true;
 							NextLineToCout = 2;
-							break;
-						case 0:
-							suffix = "";
-							NextLineCout = true;
-							NextLineToCout = 3;
 							break;
 						default:
 							break;
 						}
 					}
 
+					offset = 0;
+					//Looking for tabulator in suffix
+					while ((suffix.find("\t",offset) != string::npos) && (NextLineToCout!=2))
+					{	
+						
+						size_t tab_poz = suffix.find("\t", offset);
+
+						switch (tab_poz)
+						{
+						case 0: suffix = "\\t" + suffix.substr(1, suffix.length()); //if tabulator appear as 1 char of suffix eg: \taa
+							break;
+						case 1: suffix = suffix.substr(0, tab_poz) + "\\t" + suffix.substr(tab_poz+1, suffix.length()); //if tabulator appear as 2 char of suffix eg: a\ta
+							break;
+						case 2: suffix = suffix.substr(0, tab_poz) + "\\t" + suffix.substr(tab_poz + 1, suffix.length()); //if tabulator appear as 3 char of suffix eg: aa\t or \t\ta
+							break;
+						case 3: suffix = suffix.substr(0, tab_poz) + "\\t"; //if tabulator appear as 4 char of suffix eg: \ta\t
+							break;
+						case 4: suffix = suffix.substr(0, tab_poz) + "\\t"; //if tabulator appear as 5 char of suffix eg: \t\t\t
+							break;
+						default:
+							break;
+
+						}
+						offset = tab_poz+2;
+					}//Set suffux end here
+					//--------------------------------------------
+					//--------------------------------------------
+
+
+					//Final display
 					if (NextLineCout == false)
 					{
-						cout << this->name << "(" << charNum + occur << ")" << preffix << "..." << suffix << endl;
+						cout << this->name << "(" << charNum + occur << ")" << preffix << "..." << suffix << endl; //If all preffix and suffix has been set 
 					}
 					else
 					{
-						cout << this->name << "(" << charNum + occur << ")" << preffix << "..." << suffix;
+						cout << this->name << "(" << charNum + occur << ")" << preffix << "..." << suffix; //If suffix require haracters from next line
 					}
 					
 
-					charNum += line.length();
+					charNum += line.length(); //Caclulate total numbers of charaters
 				}
+
+				//If match not found
 				else
 				{
 					charNum += line.length();
 				}
+
+				//Store previous line
 				Prevline = line;
 			}
 
@@ -244,17 +349,15 @@ public:
 		{
 			cout << "unexpected error in: " << this->name;
 		}
-
 		file.close();
 
-
-	}
-
-
-
-
-	
+	}	
 };
+//----------------------------------------------
+//----------------End of File Class------------
+
+
+
 
 
 int main()
